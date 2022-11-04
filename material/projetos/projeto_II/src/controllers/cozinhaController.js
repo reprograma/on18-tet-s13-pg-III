7b3984f126628f21a0d6b95eaaecbf1fa56650fd
Thaysa Zooby <thaysa.zooby@gmail.com>
2022-11-04 18:27:16 -0300

    subindo as rotas e os controles conectados

--- material/projetos/projeto_II/src/controllers/cozinhaController.js
+++ material/projetos/projeto_II/src/controllers/cozinhaController.js
@@ -3,9 +3,29 @@ const mongoose = require("mongoose")
 const CozinhaSchema = require("../models/CozinhaSchema")
 
 const buscarCozinhas = async(req,res)=>{
+    const {nome} =req.query
+}
+
+const buscarCozinhaPorId = async(req, res)=>{
+    
+}
+
+const cadastrarCozinha = async(req,res)=>{
+
+}
+
+const deletarCozinha = async(req,res)=>{
+
+}
+
+const alterarCozinha = async(req, res)=>{
 
 }
 
 module.exports = {
-    buscarCozinhas
+    buscarCozinhas,
+    buscarCozinhaPorId,
+    cadastrarCozinha,
+    deletarCozinha,
+    alterarCozinha
 }
--- material/projetos/projeto_II/src/routes/cozinhaRoutes.js
+++ material/projetos/projeto_II/src/routes/cozinhaRoutes.js
@@ -4,5 +4,12 @@ const router = express.Router()
 const controller = require('../controllers/cozinhaController')
 
 router.get('/cozinha', controller.buscarCozinhas)
+router.get('/cozinha/:id', controller.buscarCozinhaPorId)
+
+router.post('/cozinha', controller.cadastrarCozinha)
+
+router.delete('/cozinha/:id', controller.deletarCozinha)
+
+router.patch('/cozinha/:id', controller.alterarCozinha)
 
 module.exports = router
