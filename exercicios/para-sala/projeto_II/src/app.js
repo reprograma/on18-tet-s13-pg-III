960272397f83a3b1c40503f9f7d01ec1212414a9
Hanna <hanna.goncalves.pro@gmail.com>
2022-11-05 11:38:10 -0300

    adicionada conexão com BD - rodando

--- exercicios/para-sala/projeto_II/src/app.js
+++ exercicios/para-sala/projeto_II/src/app.js
@@ -2,10 +2,13 @@ require('dotenv').config()
 
 const express = require('express')
 const cors = require('cors')
-const { json } = require('express')
 const app = express()
+
+const database = require('./database/mongooseConnect')
 
 app.use(cors())
 app.use(express.json())
+
+database.connect()
 
 module.exports = app
---
+++ exercicios/para-sala/projeto_II/src/database/mongooseConnect.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const uri = process.env.MONGODB_URI
+
+const connect = async ()=>{
+    try {
+        await mongoose.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        console.log('Banco Conectado!')
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+module.exports = {
+    connect
+}
