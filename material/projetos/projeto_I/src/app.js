155bf44291a7a9b94b374e1e74f11e8754db1398
hoxas <hoxas@live.com>
2022-11-05 14:59:14 -0300

    ➕Add routes to app

--- material/projetos/projeto_I/src/app.js
+++ material/projetos/projeto_I/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const bibliotecaRoutes = require("./routes/bibliotecaRoutes")
 const app = express();
 
 app.use(express.json())
@@ -8,5 +9,7 @@ app.use(cors())
 app.get("/", (req, res) => {
     res.status(200).send("API ON")
 })
+
+app.use("/biblioteca", bibliotecaRoutes)
 
 module.exports = app;
