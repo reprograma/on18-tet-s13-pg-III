b8f8eda43995aefa5673de6aff7f4cb6bab407f3
evyassis <evellynassis@outlook.com.br>
2022-11-05 13:36:16 -0300

    ajuste no model da biblioteca

--- exercicios/para-sala/projeto_I/src/models/BibliotecaModel.js
+++ exercicios/para-sala/projeto_I/src/models/BibliotecaModel.js
@@ -6,22 +6,20 @@ const BibliotecaSchema = new mongoose.Schema('Biblioteca', {
     cnpj: { type: String, required: true },
     telefone: String,
     iniciativaPrivada: Boolean,
-    endereco: [Endereco],
+    endereco: {
+        cep: {type: String, required: true},
+        rua: {type: String, required: true},
+        numero: {type: Number, required: true},
+        complemento: String,
+        referencia: String,
+        estado: {type: String, required: true},
+        cidade: {type: String, required: true},
+        bairro: {type: String, required: true}
+    },
     bairrosQueAtuam: [String],
     site: String,
     atividadesDisponiveis: [String],
     pessoaResponsavel: { type: String, required: true}
-})
-
-const Endereco = new mongoose.Schema({
-    cep: {type: String, required: true},
-    rua: {type: String, required: true},
-    numero: {type: Number, required: true},
-    complemento: String,
-    referencia: String,
-    estado: {type: String, required: true},
-    cidade: {type: String, required: true},
-    bairro: {type: String, required: true}
 })
 
 module.exports = Biblioteca = mongoose.model('Biblioteca', BibliotecaSchema)
