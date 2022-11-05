73dc9c8fc595fca2d02ad1b9e7e564f3d88d8319
hoxas <hoxas@live.com>
2022-11-05 11:43:06 -0300

    📚 Add biblioteca model

---
+++ material/projetos/projeto_I/src/models/bibliotecaModel.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const enderecoSchema = new Schema({
+    cep: {type: String, required: true},
+    rua: {type: String, required: true},
+    numero: {type: Number, required: true},
+    complemento: {type: String, required: false},
+    referencia: {type: String, required: false},
+    estado: {type: String, required: true},
+    cidade: {type: String, required: true},
+    bairro: {type: String, required: true}
+})
+
+const bibliotecaSchema = new Schema({
+    _id: mongoose.ObjectId,
+    nome: {type: String, required: true},
+    cnpj: {type: String, required: true},
+    telefone: {type: String, required: true},
+    privada: {type: Boolean, required: true},
+    endereco: {type: enderecoSchema, required: true},
+    bairros: [String],
+    site: {type: String, required: false},
+    atividades: [String],
+    responsavel: {type: String, required: true}
+})
+
+const bibliotecaModel = mongoose.model("biblioteca", bibliotecaSchema)
+
+module.exports = bibliotecaModel;
