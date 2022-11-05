318c4ec7ab4a7a7757d3b1809840b8889ec2fcd9
whoisbrizza <brizza.adv@hotmail.com>
2022-11-05 13:22:25 -0300

    Ajustes

--- exercicios/para-sala/projeto_II/server.js
+++ exercicios/para-sala/projeto_II/server.js
@@ -2,10 +2,4 @@ require('dotenv').config() //invoca o dotenv
 const app = require("./src/app") //invoca app
 const PORT = process.env.PORT // traz o valor de port do arquivo .env
 
-app.get('/', function(req, res) {
-    res.send({
-        message: 'Primeiro GET OK'
-    })
-})
-
 app.listen(PORT, () => console.log(`Projeto II - Cozinha Comunitária rodando na porta ${PORT}!`)) //define a porta do servidor
