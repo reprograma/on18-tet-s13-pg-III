353e8a6f1c6c1d676641add9bf5e9dd0c832dedf
hoxas <hoxas@live.com>
2022-11-06 00:12:05 -0300

    🆔 Add get by id + formatting

--- material/projetos/projeto_I/src/controllers/bibliotecaController.js
+++ material/projetos/projeto_I/src/controllers/bibliotecaController.js
@@ -1,41 +1,47 @@
 const mongoose = require("mongoose");
 const config = require("../../config.json");
-const Biblioteca = require("../models/bibliotecaModel")
+const Biblioteca = require("../models/bibliotecaModel");
 
-const uri = `mongodb+srv://kathalice:${config.password}@ler-e-saber.bjinctf.mongodb.net/?retryWrites=true&w=majority`
+const uri = `mongodb+srv://kathalice:${config.password}@ler-e-saber.bjinctf.mongodb.net/?retryWrites=true&w=majority`;
 
-const tryCatchWrapper = fn => {
-    return async function wrappedFn(req, res) {
-        try {
-            await fn(req, res);
-        } catch (err) {
-            res.status(500).send({
-                message: err.message
-            })
-        }
+const tryCatchWrapper = (fn) => {
+  return async function wrappedFn(req, res) {
+    try {
+      await fn(req, res);
+    } catch (err) {
+      res.status(500).send({
+        message: err.message,
+      });
     }
-}
+  };
+};
 
 const get = async (req, res) => {
-    mongoose.connect(uri)
-    const result = await Biblioteca.find(req.query)
-    res.status(200).send(result)
-}
+  mongoose.connect(uri);
+  const result = await Biblioteca.find(req.query);
+  res.status(200).send(result);
+};
+
+const getById = async (req, res) => {
+  mongoose.connect(uri);
+  const result = await Biblioteca.findById(req.params.id);
+  res.status(200).send(result);
+};
 
 const post = async (req, res) => {
-    mongoose.connect(uri);
-    const newBiblioteca = new Biblioteca({
-        _id: new mongoose.Types.ObjectId(),
-         ...req.body
-    });
-    
-    newBiblioteca.save(err => {
-        if (err) { 
-            res.status(400).send({message: err.message})
-        } else { 
-            res.status(201).send(newBiblioteca) 
-        }
-    });
-}
+  mongoose.connect(uri);
+  const newBiblioteca = new Biblioteca({
+    _id: new mongoose.Types.ObjectId(),
+    ...req.body,
+  });
 
-module.exports = { tryCatchWrapper, get, post }
+  newBiblioteca.save((err) => {
+    if (err) {
+      res.status(400).send({ message: err.message });
+    } else {
+      res.status(201).send(newBiblioteca);
+    }
+  });
+};
+
+module.exports = { tryCatchWrapper, get, getById, post };
--- material/projetos/projeto_I/src/routes/bibliotecaRoutes.js
+++ material/projetos/projeto_I/src/routes/bibliotecaRoutes.js
@@ -1,8 +1,9 @@
-const routes = require("express").Router()
-const controller = require("../controllers/bibliotecaController")
-const { tryCatchWrapper } = controller
+const routes = require("express").Router();
+const controller = require("../controllers/bibliotecaController");
+const { tryCatchWrapper } = controller;
 
 routes.get("/", tryCatchWrapper(controller.get));
 routes.post("/", tryCatchWrapper(controller.post));
+routes.get("/:id", tryCatchWrapper(controller.getById));
 
 module.exports = routes;
