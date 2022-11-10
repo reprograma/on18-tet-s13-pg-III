c9c6398fe59f8bd2a94ad62bc6ef9a95f144d357
whoisbrizza <brizza.adv@hotmail.com>
2022-11-10 12:46:56 -0300

    Projeto concluído

--- exercicios/para-sala/projeto_II/server.js
+++ exercicios/para-sala/projeto_II/server.js
@@ -1,5 +1,7 @@
-require('dotenv').config() //invoca o dotenv
-const app = require("./src/app") //invoca app
-const PORT = process.env.PORT // traz o valor de port do arquivo .env
+require('dotenv').config();
 
-app.listen(PORT, () => console.log(`Projeto II - Cozinha Comunitária rodando na porta ${PORT}!`)) //define a porta do servidor
+const app = require("./src/app");
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => console.log(`Projeto II - Cozinha Comunitária rodando na porta ${PORT}!`))
--- exercicios/para-sala/projeto_II/src/app.js
+++ exercicios/para-sala/projeto_II/src/app.js
@@ -1,9 +1,17 @@
-require('dotenv').config() // invoca o dotenv
-const express = require("express") //invoca o express
-const cors = require("cors") //invoca o cors
-const app = express() // executa o express
-const database = require('./database/mongoConfig') //invoca o arquivo mongoConfig da database
-app.use(express.json()) // transformar tudo em json *
-app.use(cors()) //executar cors
-database.connect() //executando a função connect do arquivo mongoConfig
-module.exports = app //exporta o arquivo app.j
+require('dotenv').config()
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const database = require('./database/mongoConfig');
+const cozinhaRoutes = require("./routes/cozinhaRoutes");
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/cozinha", cozinhaRoutes);
+
+database.connect();
+
+module.exports = app;
