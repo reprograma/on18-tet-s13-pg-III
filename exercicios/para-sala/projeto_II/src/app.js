f32020149b4ba408b22e21d59ddb80d80c56fe87
TEBE 852 <alaneidepimenta1997@gmail.com>
2022-11-10 12:53:53 -0300

    Projeto finalizado

--- exercicios/para-sala/projeto_II/src/app.js
+++ exercicios/para-sala/projeto_II/src/app.js
@@ -1,10 +1,19 @@
-require('dotenv').config() // invoca o dotenv
-const express = require("express") //invoca o express
-const cors = require("cors") //invoca o cors
-const app = express() // executa o express
-const database = require('./database/mongoConfig') //invoca o arquivo mongoConfig da database
-app.use(express.json()) // transformar tudo em json *
-app.use(cors()) //executar cors
-database.connect() //executando a função connect do arquivo mongoConfig
-module.exports = app //exp
 
+
+require('dotenv').config()
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const database = require('./database/mongoConfig');
+const cozinhaRoutes = require("./routes/cozinhaRouter");
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/cozinha", cozinhaRoutes);
+
+database.connect();
+
+module.exports = app;
---
+++ exercicios/para-sala/projeto_II/src/routes/cozinhaRouter.js
@@ -0,0 +1,11 @@
+const rotas = require("express").Router()
+const controller = require("../controllers/cozinhaController")
+
+rotas.get("/", controller.buscarTodasCozinhas)
+rotas.get("/:id", controller.buscarCozinhaId)
+rotas.post("/", controller.criarCozinha)
+rotas.delete("/:id", controller.deletarCozinha)
+rotas.patch("/:id", controller.atualizarCozinha)
+
+
+module.exports = rotas
