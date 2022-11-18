78a9f9e7c77b37631756721856dd5af387306888
julianavirgens88 <julianalv1@hotmail.com>
2022-11-18 17:09:49 -0300

    acrescentar patch

--- exercicios/para-casa/resolucao/src/routes/cozinhaRoutes.js
+++ exercicios/para-casa/resolucao/src/routes/cozinhaRoutes.js
@@ -3,9 +3,10 @@ const router = express.Router();
 
 const controller = require('../controllers/cozinhaControllers');
 
-router.post('/criar', controller.criarCozinha),
+router.post('/criar', controller.criarCozinha);
 router.get('/buscar', controller.buscarTodasCozinhas);
-router.delete('/:id', controller.deletarCozinha)
+router.delete('/:id', controller.deletarCozinha);
+router.patch("/:id", controller.atualizarCozinha);
 
 
 module.exports = router;
